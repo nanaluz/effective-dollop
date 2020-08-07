8a39a7c9f45f8426ef66876a95a591f8516205a7
nanaluz <63294705+nanaluz@users.noreply.github.com>
2020-08-08 04:09:51 +0800

    Initialize move_precondition_to_constructor

---
+++ firebase-firestore/src/test/java/com/google/firebase/firestore/local/MemoryLocalStoreTest.java
@@ -0,0 +1,53 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.firebase.firestore.local;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.runner.RunWith;
+import org.robolectric.ParameterizedRobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(ParameterizedRobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class MemoryLocalStoreTest extends LocalStoreTestCase {
+
+  private QueryEngine queryEngine;
+
+  @ParameterizedRobolectricTestRunner.Parameters(name = "QueryEngine = {0}")
+  public static Collection<Object[]> data() {
+    return Arrays.asList(
+        new Object[] {new SimpleQueryEngine()}, new Object[] {new IndexFreeQueryEngine()});
+  }
+
+  public MemoryLocalStoreTest(QueryEngine queryEngine) {
+    this.queryEngine = queryEngine;
+  }
+
+  @Override
+  Persistence getPersistence() {
+    return PersistenceTestHelpers.createEagerGCMemoryPersistence();
+  }
+
+  @Override
+  QueryEngine getQueryEngine() {
+    return this.queryEngine;
+  }
+
+  @Override
+  boolean garbageCollectorIsEager() {
+    return true;
+  }
+}
