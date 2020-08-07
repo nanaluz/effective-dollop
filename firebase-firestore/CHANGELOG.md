e76e29cf3b47871ad7ba05830d3611cd3bc733b5
nanaluz <63294705+nanaluz@users.noreply.github.com>
2020-08-08 02:34:27 +0800

    Initialize ashwin.test_lab

---
+++ firebase-firestore/CHANGELOG.md
@@ -0,0 +1,94 @@
+# Unreleased
+- [feature] Added `FieldValue.increment()`, which can be used in `update()`
+  and `set(..., SetOptions.merge())` to increment or decrement numeric field
+  values safely without transactions.
+- [fixed] Fixed calculation of SQLite database size on Android 9 Pie devices.
+  Previous method could be off by a few MBs on these devices, potentially
+  delaying garbage collection.
+- [changed] Prepared the persistence layer to support collection group queries.
+  While this feature is not yet available, all schema changes are included
+  in this release.
+- [changed] Added `@RestrictTo` annotations to discourage the use of APIs that
+  are not public. This affects internal APIs that were previously obfuscated
+  and are not mentioned in our documentation.
+- [changed] Improved error messages for certain Number types that are not
+  supported by our serialization layer (#272).
+
+# 18.0.1
+- [fixed] Fixed an issue where Firestore would crash if handling write batches
+  larger than 2 MB in size (#208).
+- [changed] Firestore now recovers more quickly from long periods without
+  network access (#217).
+
+# 18.0.0
+- [changed] The `timestampsInSnapshotsEnabled` setting is now enabled by
+  default. Timestamp fields that read from a `DocumentSnapshot` are now
+  returned as `Timestamp` objects instead of `Date` objects. This is a breaking
+  change; developers must update any code that expects to receive a `Date`
+  object. See https://firebase.google.com/docs/reference/android/com/google/firebase/firestore/FirebaseFirestoreSettings.Builder.html#setTimestampsInSnapshotsEnabled(boolean) for more details.
+- [feature] Custom objects (POJOs) can now be passed in several ways: as a
+  field value in `update()`, within `Map<>` objects passed to `set()`, in array
+  transform operations, and in query filters.
+- [feature] `DocumentSnapshot.get()` now supports retrieving fields as
+  custom objects (POJOs) by passing a `Class<T>` instance, e.g.,
+  `snapshot.get("field", CustomType.class)`.
+- [fixed] Fixed an issue where if an app sent a write to the server, but the
+  app was shut down before a listener received the write, the app could crash.
+
+# 17.1.5
+- [changed] Firestore now recovers more quickly from bad network states.
+- [changed] Improved performance for reading large collections.
+- [fixed] Offline persistence now properly records schema downgrades. This is a
+  forward-looking change that allows you to safely downgrade from future SDK
+  versions to this version (v17.1.5). You can already safely downgrade versions
+  now depending on the source version. For example, you can safely downgrade
+  from v17.1.4 to v17.1.2 because there are no schema changes between those
+  versions. (#134)
+
+# 17.1.4
+- [fixed] Fixed a SQLite transaction handling issue that occasionally masked
+  exceptions when Firestore closed a transaction that was never started. For
+  more information, see the issue report in GitHub (https://github.com/firebase/firebase-android-sdk/issues/115).
+- [fixed] Fixed a race condition that caused a `SQLiteDatabaseLockedException`
+  when an app attempted to access the SQLite database from multiple threads.
+
+# 17.1.2
+- [changed] Changed how the SDK handles locally-updated documents while syncing
+  those updates with Cloud Firestore servers. This can lead to slight behavior
+  changes and may affect the `SnapshotMetadata.hasPendingWrites()` metadata
+  flag.
+- [changed] Eliminated superfluous update events for locally cached documents
+  that are known to lag behind the server version. Instead, the SDK buffers
+  these events until the client has caught up with the server.
+
+# 17.1.1
+- [fixed] Fixed an issue where the first `get()` call made after being offline
+  could incorrectly return cached data without attempting to reach the backend.
+- [changed] Changed `get()` to only make one attempt to reach the backend before
+  returning cached data, potentially reducing delays while offline.
+- [fixed] Fixed an issue that caused Firebase to drop empty objects from calls
+  to `set(..., SetOptions.merge())`.
+- [fixed] Updated printf-style templates to ensure that they're compile time
+  constants. Previously, some were influenced by error messages. When those
+  error messages contained `%p` or other, related tokens, `String.format()`
+  would throw an exception.
+- [changed] Some SDK errors that represent common mistakes, like permission
+  errors or missing indexes, are automatically be logged as warnings in addition
+  to being surfaced via the API.
+
+# 17.1.0
+- [feature] Added `FieldValue.arrayUnion()` and `FieldValue.arrayRemove()` to
+  atomically add and remove elements from an array field in a document.
+- [feature] Added `Query.whereArrayContains()` query operator to find documents
+  where an array field contains a specific element.
+
+# 17.0.4
+- [fixed] Fixed an issue where queries returned fewer results than they should,
+  caused by documents that were cached as deleted when they should not have
+  been (firebase/firebase-ios-sdk#1548). Some cache data is cleared and so
+  clients may use extra bandwidth the first time they launch with this version
+  of the SDK.
+
+# 17.0.3
+- [changed] The `Timestamp` class now implements `Parcelable` in addition to
+  `Comparable`.
---
+++ firebase-firestore/src/test/java/com/google/firebase/firestore/local/SQLiteSchemaTest.java
@@ -0,0 +1,372 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.firebase.firestore.local;
+
+import static com.google.firebase.firestore.local.EncodedPath.decodeResourcePath;
+import static com.google.firebase.firestore.local.EncodedPath.encode;
+import static com.google.firebase.firestore.testutil.TestUtil.map;
+import static com.google.firebase.firestore.testutil.TestUtil.path;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.google.firebase.firestore.model.DatabaseId;
+import com.google.firebase.firestore.model.ResourcePath;
+import com.google.firebase.firestore.proto.WriteBatch;
+import com.google.firestore.v1.Document;
+import com.google.firestore.v1.Write;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+/** Tests migrations in SQLiteSchema. */
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class SQLiteSchemaTest {
+
+  private static final String[] NO_ARGS = new String[0];
+
+  private SQLiteDatabase db;
+  private SQLiteSchema schema;
+
+  @Before
+  public void setUp() {
+    SQLiteOpenHelper opener =
+        new SQLiteOpenHelper(RuntimeEnvironment.application, "foo", null, 1) {
+          @Override
+          public void onCreate(SQLiteDatabase db) {}
+
+          @Override
+          public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+        };
+    db = opener.getWritableDatabase();
+    schema = new SQLiteSchema(db);
+  }
+
+  @After
+  public void tearDown() {
+    if (db != null) {
+      db.close();
+    }
+  }
+
+  @Test
+  public void canRerunMigrations() {
+    schema.runMigrations();
+    // Run the whole thing again
+    schema.runMigrations();
+    // Run just a piece. Adds a column, make sure it doesn't throw
+    schema.runMigrations(4, 6);
+  }
+
+  private Map<String, Set<String>> getCurrentSchema() {
+    Map<String, Set<String>> tables = new HashMap<>();
+    new SQLitePersistence.Query(db, "SELECT tbl_name FROM sqlite_master WHERE type = \"table\"")
+        .forEach(
+            c -> {
+              String table = c.getString(0);
+              Set<String> columns = new HashSet<>(schema.getTableColumns(table));
+              tables.put(table, columns);
+            });
+    return tables;
+  }
+
+  private void assertNoRemovals(
+      Map<String, Set<String>> oldSchema, Map<String, Set<String>> newSchema, int newVersion) {
+    for (Map.Entry<String, Set<String>> entry : oldSchema.entrySet()) {
+      String table = entry.getKey();
+      Set<String> newColumns = newSchema.get(table);
+      assertNotNull("Table " + table + " was deleted at version " + newVersion, newColumns);
+      Set<String> oldColumns = entry.getValue();
+      // We could use `Set.containsAll()`, but if we iterate we can point out the column that was
+      // deleted.
+      for (String column : oldColumns) {
+        assertTrue(
+            "Column " + column + " was deleted from table " + table + " at version " + newVersion,
+            newColumns.contains(column));
+      }
+    }
+  }
+
+  @Test
+  public void migrationsDontDeleteTablesOrColumns() {
+    // In order to support users downgrading the SDK we need to make sure that every prior-released
+    // version of the SDK can gracefully handle running against an upgraded schema. We can't
+    // guarantee this in the general case, but this test at least ensures that no schema upgrade
+    // deletes an existing table or column, which would be very likely to break old versions of the
+    // SDK relying on that table or column.
+    Map<String, Set<String>> tables = new HashMap<>();
+    for (int toVersion = 1; toVersion <= SQLiteSchema.VERSION; toVersion++) {
+      schema.runMigrations(toVersion - 1, toVersion);
+      Map<String, Set<String>> newTables = getCurrentSchema();
+      assertNoRemovals(tables, newTables, toVersion);
+      tables = newTables;
+    }
+  }
+
+  @Test
+  public void canRecoverFromDowngrades() {
+    for (int downgradeVersion = 0; downgradeVersion < SQLiteSchema.VERSION; downgradeVersion++) {
+      // Upgrade schema to current, then upgrade from `downgradeVersion` to current
+      schema.runMigrations();
+      schema.runMigrations(downgradeVersion, SQLiteSchema.VERSION);
+    }
+  }
+
+  @Test
+  public void createsMutationsTable() {
+    schema.runMigrations();
+
+    assertNoResultsForQuery("SELECT uid, batch_id FROM mutations", NO_ARGS);
+
+    db.execSQL("INSERT INTO mutations (uid, batch_id) VALUES ('foo', 1)");
+
+    Cursor cursor = db.rawQuery("SELECT uid, batch_id FROM mutations", NO_ARGS);
+    assertTrue(cursor.moveToFirst());
+    assertEquals("foo", cursor.getString(cursor.getColumnIndex("uid")));
+    assertEquals(1, cursor.getInt(cursor.getColumnIndex("batch_id")));
+
+    assertFalse(cursor.moveToNext());
+    cursor.close();
+  }
+
+  @Test
+  public void deletesAllTargets() {
+    schema.runMigrations(0, 2);
+
+    db.execSQL("INSERT INTO targets (canonical_id, target_id) VALUES ('foo1', 1)");
+    db.execSQL("INSERT INTO targets (canonical_id, target_id) VALUES ('foo2', 2)");
+    db.execSQL("INSERT INTO target_globals (highest_target_id) VALUES (2)");
+
+    db.execSQL("INSERT INTO target_documents (target_id, path) VALUES (1, 'foo/bar')");
+    db.execSQL("INSERT INTO target_documents (target_id, path) VALUES (2, 'foo/baz')");
+
+    schema.runMigrations(2, 3);
+
+    assertNoResultsForQuery("SELECT * FROM targets", NO_ARGS);
+    assertNoResultsForQuery("SELECT * FROM target_globals", NO_ARGS);
+    assertNoResultsForQuery("SELECT * FROM target_documents", NO_ARGS);
+  }
+
+  @Test
+  public void countsTargets() {
+    schema.runMigrations(0, 3);
+    long expected = 50;
+    for (int i = 0; i < expected; i++) {
+      db.execSQL(
+          "INSERT INTO targets (canonical_id, target_id) VALUES (?, ?)",
+          new String[] {"foo" + i, "" + i});
+    }
+    schema.runMigrations(3, 5);
+    Cursor c = db.rawQuery("SELECT target_count FROM target_globals LIMIT 1", NO_ARGS);
+    assertTrue(c.moveToFirst());
+    long targetCount = c.getInt(0);
+    assertEquals(expected, targetCount);
+  }
+
+  @Test
+  public void testDatabaseName() {
+    assertEquals(
+        "firestore.%5BDEFAULT%5D.my-project.%28default%29",
+        SQLitePersistence.databaseName("[DEFAULT]", DatabaseId.forProject("my-project")));
+    assertEquals(
+        "firestore.%5BDEFAULT%5D.my-project.my-database",
+        SQLitePersistence.databaseName(
+            "[DEFAULT]", DatabaseId.forDatabase("my-project", "my-database")));
+  }
+
+  @Test
+  public void dropsHeldWriteAcks() {
+    // This test creates a database with schema version 5 that has two users, both of which have
+    // acknowledged mutations that haven't yet been removed from IndexedDb ("heldWriteAcks").
+    // Schema version 6 removes heldWriteAcks, and as such these mutations are deleted.
+    schema.runMigrations(0, 5);
+
+    // User 'userA' has two acknowledged mutations and one that is pending.
+    // User 'userB' has one acknowledged mutation and one that is pending.
+    addMutationBatch(db, 1, "userA", "docs/foo");
+    addMutationBatch(db, 2, "userA", "docs/foo");
+    addMutationBatch(db, 3, "userB", "docs/bar", "doc/baz");
+    addMutationBatch(db, 4, "userB", "docs/pending");
+    addMutationBatch(db, 5, "userA", "docs/pending");
+
+    // Populate the mutation queues' metadata
+    db.execSQL(
+        "INSERT INTO mutation_queues (uid, last_acknowledged_batch_id) VALUES (?, ?)",
+        new Object[] {"userA", 2});
+    db.execSQL(
+        "INSERT INTO mutation_queues (uid, last_acknowledged_batch_id) VALUES (?, ?)",
+        new Object[] {"userB", 3});
+    db.execSQL(
+        "INSERT INTO mutation_queues (uid, last_acknowledged_batch_id) VALUES (?, ?)",
+        new Object[] {"userC", -1});
+
+    schema.runMigrations(5, 6);
+
+    // Verify that all but the two pending mutations have been cleared by the migration.
+    new SQLitePersistence.Query(db, "SELECT COUNT(*) FROM mutations")
+        .first(value -> assertEquals(2, value.getInt(0)));
+
+    // Verify that we still have two index entries for the pending documents
+    new SQLitePersistence.Query(db, "SELECT COUNT(*) FROM document_mutations")
+        .first(value -> assertEquals(2, value.getInt(0)));
+
+    // Verify that we still have one metadata entry for each existing queue
+    new SQLitePersistence.Query(db, "SELECT COUNT(*) FROM mutation_queues")
+        .first(value -> assertEquals(3, value.getInt(0)));
+  }
+
+  private void addMutationBatch(SQLiteDatabase db, int batchId, String uid, String... docs) {
+    WriteBatch.Builder write = WriteBatch.newBuilder();
+    write.setBatchId(batchId);
+
+    for (String doc : docs) {
+      db.execSQL(
+          "INSERT INTO document_mutations (uid, path, batch_id) VALUES (?, ?, ?)",
+          new Object[] {uid, encode(ResourcePath.fromString(doc)), batchId});
+
+      write.addWrites(
+          Write.newBuilder()
+              .setUpdate(
+                  Document.newBuilder()
+                      .setName("projects/projectId/databases/(default)/documents/" + doc)));
+    }
+
+    db.execSQL(
+        "INSERT INTO mutations (uid, batch_id, mutations) VALUES (?,?,?)",
+        new Object[] {uid, batchId, write.build().toByteArray()});
+  }
+
+  @Test
+  public void addsSentinelRows() {
+    schema.runMigrations(0, 6);
+
+    long oldSequenceNumber = 1;
+    // Set the highest sequence number to this value so that untagged documents
+    // will pick up this value.
+    long newSequenceNumber = 2;
+    db.execSQL(
+        "UPDATE target_globals SET highest_listen_sequence_number = ?",
+        new Object[] {newSequenceNumber});
+
+    // Set up some documents (we only need the keys)
+    // For the odd ones, add sentinel rows.
+    for (int i = 0; i < 10; i++) {
+      String path = "docs/doc_" + i;
+      db.execSQL("INSERT INTO remote_documents (path) VALUES (?)", new String[] {path});
+      if (i % 2 == 1) {
+        db.execSQL(
+            "INSERT INTO target_documents (target_id, path, sequence_number) VALUES (0, ?, ?)",
+            new Object[] {path, oldSequenceNumber});
+      }
+    }
+
+    schema.runMigrations(6, 7);
+
+    // Verify.
+    new SQLitePersistence.Query(
+            db, "SELECT path, sequence_number FROM target_documents WHERE target_id = 0")
+        .forEach(
+            row -> {
+              String path = row.getString(0);
+              long sequenceNumber = row.getLong(1);
+
+              int docNum = Integer.parseInt(path.split("_", -1)[1]);
+              // The even documents were missing sequence numbers, they should now be filled in
+              // to have the new sequence number. The odd documents should have their
+              // sequence number unchanged, and so be the old value.
+              long expected = docNum % 2 == 1 ? oldSequenceNumber : newSequenceNumber;
+              assertEquals(expected, sequenceNumber);
+            });
+  }
+
+  @Test
+  public void canCreateCollectionParentsIndex() {
+    // This test creates a database with schema version 7 that has a few
+    // mutations and a few remote documents and then ensures that appropriate
+    // entries are written to the collectionParentIndex.
+    List<String> writePaths = asList("cg1/x", "cg1/y", "cg1/x/cg1/x", "cg2/x", "cg1/x/cg2/x");
+    List<String> remoteDocPaths =
+        asList("cg1/z", "cg1/y/cg1/x", "cg2/x/cg3/x", "blah/x/blah/x/cg3/x");
+    Map<String, List<String>> expectedParents =
+        map(
+            "cg1", asList("", "cg1/x", "cg1/y"),
+            "cg2", asList("", "cg1/x"),
+            "cg3", asList("blah/x/blah/x", "cg2/x"));
+
+    schema.runMigrations(0, 7);
+    // Write mutations.
+    int batchId = 1;
+    for (String writePath : writePaths) {
+      addMutationBatch(db, batchId++, "user", writePath);
+    }
+    // Write remote document entries.
+    for (String remoteDocPath : remoteDocPaths) {
+      db.execSQL(
+          "INSERT INTO remote_documents (path) VALUES (?)",
+          new String[] {encode(path(remoteDocPath))});
+    }
+
+    // Migrate to v8 and verify index entries.
+    schema.runMigrations(7, 8);
+    Map<String, List<String>> actualParents = new HashMap<>();
+    new SQLitePersistence.Query(db, "SELECT collection_id, parent FROM collection_parents")
+        .forEach(
+            row -> {
+              String collectionId = row.getString(0);
+              String parent = decodeResourcePath(row.getString(1)).toString();
+              List<String> parents = actualParents.get(collectionId);
+              if (parents == null) {
+                parents = new ArrayList<>();
+                actualParents.put(collectionId, parents);
+              }
+              parents.add(parent);
+            });
+
+    // Sort results.
+    for (List<String> parents : actualParents.values()) {
+      parents.sort(String::compareTo);
+    }
+
+    assertEquals(expectedParents, actualParents);
+  }
+
+  private void assertNoResultsForQuery(String query, String[] args) {
+    Cursor cursor = null;
+    try {
+      cursor = db.rawQuery(query, args);
+      assertFalse(cursor.moveToFirst());
+    } finally {
+      if (cursor != null) {
+        cursor.close();
+      }
+    }
+  }
+}
